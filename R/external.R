b77dd7457307d9d32706f8fda6f8823d047dfc46
Marcin Cieslik <mcieslik@med.umich.edu>
2018-07-06 03:19:07 +0000

    fix hardcoded path

--- R/external.R
+++ R/external.R
@@ -7,7 +7,7 @@
         stop("by should be a file or window-size")
     }
     prefix <- tempfile("mosdepth_")
-    ret <- system2("tools/bin/mosdepth", sprintf("-F 772 -n -t4 -b %s %s %s", by, prefix, bam))
+    ret <- system2("mosdepth", sprintf("-F 772 -n -t4 -b %s %s %s", by, prefix, bam))
     out.fn <- list.files(dirname(prefix), paste0(basename(prefix), ".regions.bed.gz$"), full.names = TRUE)
     if (!ret & file.exists(out.fn)) {
         out <- fread(paste("zcat", out.fn))[[out.col]]
